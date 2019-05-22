0d4ad133dd123fde799890d504e0d562889b03af
Roman Kuba <roman.kuba@gmail.com>
2019-05-22 09:30:23 +0200

    lesson1

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   transform: {
     "^.+\\.js$": "babel-jest",
+    "^.+\\.vue$": "vue-jest"
   },
   snapshotSerializers: [
     "<rootDir>/node_modules/jest-serializer-vue"
---
+++ specs/test.spec.js
@@ -0,0 +1,6 @@
+import TestComponent from '@/test'
+import { mount } from '@vue/test-utils'
+
+test('first-spec', () => {
+  console.log(TestComponent)
+})
