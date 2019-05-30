6b1f6eeb6bc5b10ecbad6194569599779e7dbb92
Roman Kuba <roman.kuba@gmail.com>
2019-05-30 10:53:09 +0200

    lesson6

--- specs/alert.spec.js
+++ specs/alert.spec.js
@@ -1,2 +1,28 @@
 import AlertMessage from '@/alert-message'
 import { mount } from '@vue/test-utils'
+
+jest.useFakeTimers()
+
+describe('lifecycle methods', () => {
+  test('mounted assigns interval', () => {
+    const wrapper = mount(AlertMessage)
+    expect(wrapper.vm.interval).not.toBe(undefined)
+  })
+
+  test('counter works', () => {
+    const wrapper = mount(AlertMessage)
+    expect(wrapper.vm.counter).toBe(0)
+    jest.advanceTimersByTime(1000)
+    expect(wrapper.vm.counter).toBe(1)
+    jest.advanceTimersByTime(1000)
+    expect(wrapper.vm.counter).toBe(2)
+  })
+
+  test('instance gets destroyed', () => {
+    const beforeDestroyedSpy = jest.spyOn(AlertMessage, 'beforeDestroy')
+    const wrapper = mount(AlertMessage)
+    wrapper.vm.counter = wrapper.vm.timer - 1
+    jest.advanceTimersByTime(1000)
+    expect(beforeDestroyedSpy).toHaveBeenCalled()
+  })
+})
