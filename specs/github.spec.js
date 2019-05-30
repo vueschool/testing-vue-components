7d68d10f2f38f3e688a103e1c59f04393c4d7382
Roman Kuba <roman.kuba@gmail.com>
2019-05-30 09:46:40 +0200

    lesson5

--- specs/github.spec.js
+++ specs/github.spec.js
@@ -2,5 +2,29 @@ import GithubCard from '@/github-card'
 import { mount } from '@vue/test-utils'
 
 describe('methods', () => {
+  test('composeUrl', () => {
+    const { composeUrl } = GithubCard.methods
+    expect(composeUrl(123)).toBe('https://api.github.com/users/123')
+    expect(composeUrl('freddyMercury')).toBe('https://api.github.com/users/freddyMercury')
+  })
+
+  test('fetchData', async () => {
+    const jsonMock = jest.fn().mockResolvedValue('GITHUB DATA')
+    window.fetch = jest.fn().mockResolvedValue({
+      json: jsonMock
+    })
+
+    const wrapper = mount(GithubCard, {
+      methods: {
+        composeUrl: () => 'url'
+      }
+    })
+
+    await wrapper.vm.fetchData()
+
+    expect(window.fetch).toHaveBeenCalledWith('url')
+    expect(jsonMock).toHaveBeenCalled()
+    expect(wrapper.vm.data).toBe('GITHUB DATA')
+  })
 
 })
