1b3ec258a7b8ce1267322074f6765ad8c4bc2865
Roman Kuba <rkuba@gitlab.com>
2020-02-15 14:59:13 +0100

    Added Exercise 1 base files

---
+++ specs/exercise-1.spec.js
@@ -0,0 +1,2 @@
+import UserList from '@/exercise-1';
+import { mount } from '@vue/test-utils';
---
+++ src/exercise-1.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input type="text" v-model="filter" />
+    <ul>
+      <li v-for="user in filteredUsers" :key="user">{{ user }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    users: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      filter: ''
+    };
+  },
+  computed: {
+    filteredUsers() {
+      console.log(this.users);
+      if (this.filter.trim().length === 0) return this.users;
+      return this.users.filter(user => {
+        return user.toLowerCase().includes(this.filter.toLowerCase());
+      });
+    }
+  }
+};
+</script>
